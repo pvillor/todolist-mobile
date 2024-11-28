519e48d534e23293e444fb3b318c03c7fb54057c
paulo.guerra <paulo.guerra@flex-am.com.br>
2024-11-28 10:40:52 -0400

    feat: create tasks form

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,5 +1,6 @@
-import { Image, Text, View } from "react-native";
+import { Image, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
+import { Feather } from "@expo/vector-icons"
 
 export function Home() {
   return (
@@ -7,7 +8,13 @@ export function Home() {
       <Image source={require('../../../assets/logo.png')} style={styles.logo} />
 
       <View style={styles.tasks}>
-        
+        <View style={styles.form}>
+          <TextInput style={styles.input} placeholder="Adicione uma nova tarefa" placeholderTextColor="#808080" />
+
+          <TouchableOpacity style={styles.button}>
+            <Feather name="plus-circle" color="#F2F2F2" size={16} />
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   )
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -15,6 +15,33 @@ export const styles = StyleSheet.create({
   tasks: {
     flex: 1,
     width: '100%',
-    backgroundColor: '#1A1A1A'
-  }
+    backgroundColor: '#1A1A1A',
+    alignItems: 'center'
+  },
+
+  form: {
+    width: '100%',
+    paddingLeft: 32,
+    paddingRight: 32,
+    flexDirection: 'row',
+    gap: 4,
+  },
+  
+  input: {
+    flex: 1,
+    height: 56,
+    padding: 16,
+    color: '#F2F2F2',
+    backgroundColor: '#262626',
+    borderRadius: 6,
+  },
+
+  button: {
+    width: 52,
+    height: 52,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#1E6F9F',
+    borderRadius: 6,
+  },
 })
