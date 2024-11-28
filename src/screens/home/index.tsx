521a0f65f6f81c38e23a60d47bcbc9e523c2953d
paulo.guerra <paulo.guerra@flex-am.com.br>
2024-11-28 08:09:52 -0400

    first commit

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Link Start</Text>
+    </View>
+  )
+}
